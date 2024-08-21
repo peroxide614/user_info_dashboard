c7a8218533fc3a5c70c503da36eb8beddcc5d623
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-21 19:55:24 +0300

    structure created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,42 @@ import './App.css';
 // import redicon from './image/red-circle.png';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="full-page">
+      <div className="element-wrapper">
+        <div className="title">Personal Info</div>
+        <div className="box">
+          <div>
+            <img></img>
+            <div>Username</div>
+            <div>superadmin</div>
+          </div>
+          <div className="divider"></div>
+          <div>
+            <img></img>
+            <div>Role</div>
+            <div>Admin</div>
+          </div>
+          <div className="divider"></div>
+          <div>
+            <img></img>
+            <div>Phone Number</div>
+            <div>+971525450937</div>
+          </div>
+          <div className="divider"></div>
+          <div>
+            <img></img>
+            <div>Email</div>
+            <div>superadmin@sa.intech</div>
+          </div>
+          <div className="divider"></div>
+          <div>
+            <button>Log Out</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
